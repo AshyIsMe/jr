2a5111cabe4273c9177cea46e2e0d43a543a3c04
Aaron Ash <aaron.ash@gmail.com>
2022-08-16 23:43:26 +1000

    parse numbers

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,98 @@ enum Token {
     LP,
     RP,
     Verb(String),
+    LiteralNumberArray(String),
+    LiteralString(String),
 }
 
-fn scan(sentence: &str) -> Option<Vec<Token>> {
-    let mut tokens: Vec<Token> = Vec::new();
-    let mut vs: usize = usize::MAX; //verbstart
-    let mut ve: usize = usize::MAX; //verbend
-    let mut wordend: bool = false;
-    let mut newToken: Option<Token> = None;
+#[derive(Debug)]
+struct ParseError {
+    message: String,
+}
 
+fn scan(sentence: &str) -> Result<Vec<Token>, ParseError> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut ws: usize = usize::MAX; //word start
+    let mut we: usize = usize::MAX; //word end
+    let mut word_end: bool;
+    let mut new_token: Option<Token>;
+
+    let mut ns: usize = usize::MAX; //number array start
+    let mut ne: usize = usize::MAX; //number array end
+    let mut num_end: bool;
+
+    //TODO recursive descent instead of a dumb loop
     for (i, c) in sentence.chars().enumerate() {
-        wordend = false;
-        newToken = None;
+        word_end = false;
+        new_token = None;
+        num_end = false;
         match c {
             '(' => {
-                wordend = true;
-                newToken = Some(Token::LP)
+                word_end = true;
+                num_end = true;
+                new_token = Some(Token::LP)
             }
             ')' => {
-                wordend = true;
-                newToken = Some(Token::RP)
+                word_end = true;
+                num_end = true;
+                new_token = Some(Token::RP)
             }
-            ' ' => wordend = true,
-            '\n' => wordend = true,
-            _ => match vs {
+            '\n' => {
+                word_end = true;
+                num_end = true
+            }
+            ' ' | '\t' => {
+                word_end = true
+                //num_end = true
+            }
+            '0'..='9' => match ns {
                 usize::MAX => {
-                    vs = i; //new word started
-                    ve = i
+                    ns = i; //new number array started
+                    ne = i
                 }
-                _ => ve = i, //word continued
+                _ => ne = i, //number continued
             },
+            '.' => {
+                match ns {
+                    usize::MAX => {
+                        //not in a number
+                        match ws {
+                            usize::MAX => {
+                                ws = i; //new word started
+                                we = i
+                            }
+                            _ => we = i, //word continued
+                        }
+                    }
+                    _ => ne = i, //number continued
+                }
+            }
+            _ => {
+                num_end = true;
+                match ws {
+                    usize::MAX => {
+                        ws = i; //new word started
+                        we = i
+                    }
+                    _ => we = i, //word continued
+                }
+            }
         }
-        if wordend && (vs < usize::MAX) {
-            tokens.push(Token::Verb(String::from(&sentence[vs..=ve])));
-            vs = usize::MAX;
-            ve = usize::MAX
+        if word_end && (ws < usize::MAX) {
+            tokens.push(Token::Verb(String::from(&sentence[ws..=we])));
+            ws = usize::MAX;
+            we = usize::MAX
+        } else if num_end && (ns < usize::MAX) {
+            tokens.push(Token::LiteralNumberArray(String::from(&sentence[ns..=ne])));
+            ns = usize::MAX;
+            ne = usize::MAX
         }
-        match newToken {
+        match new_token {
             Some(t) => tokens.push(t),
-            None => ()
+            None => (),
         }
     }
-    if tokens.is_empty() {
-        None
-    } else {
-        Some(tokens)
-    }
+    Ok(tokens)
 }
 
 fn main() -> io::Result<()> {
@@ -69,7 +115,6 @@ fn main() -> io::Result<()> {
             "exit" => break,
             _sentence => {
                 let tokens = scan(&buffer);
-                //println!("que? '{}'", buffer.trim());
                 println!("tokens: {:?}", tokens);
                 buffer = String::from("");
             }
