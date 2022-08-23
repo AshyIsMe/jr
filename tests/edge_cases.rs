d82f0f4580a2752bc9b7f583fa879cc5e2bcfe0f
Aaron Ash <aaron.ash@gmail.com>
2022-08-23 19:42:14 +1000

    fix: scanning trailing chars

--- tests/edge_cases.rs
+++ tests/edge_cases.rs
@@ -1,17 +1,104 @@
-#[test]
-fn invalid_one_letter() {
-    // TODO: error matcher / diagnostics
-    assert!(jr::scan("q").is_err());
-}
 
-#[test]
-fn invalid_comma() {
-    // TODO: error matcher / diagnostics
-    assert!(jr::scan(",").is_err());
-}
+use jr::{Token, scan};
 
 #[test]
 fn invalid_prime() {
     // TODO: error matcher / diagnostics
     assert!(jr::scan("'").is_err());
+}
+
+#[test]
+fn test_scan_num() {
+    let tokens = scan("1 2 _3\n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(tokens, [Token::LitNumArray(String::from("1 2 _3"))]);
+}
+
+#[test]
+fn test_scan_string() {
+    let tokens = scan("'abc'").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(tokens, [Token::LitString(String::from("abc"))]);
+}
+
+#[test]
+fn test_scan_name() {
+    let tokens = scan("abc\n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(tokens, [Token::Name(String::from("abc"))]);
+}
+
+#[test]
+fn test_scan_name_verb_name() {
+    let tokens = scan("foo + bar\n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(
+        tokens,
+        [
+            Token::Name(String::from("foo")),
+            Token::Primitive(String::from("+")),
+            Token::Name(String::from("bar")),
+        ]
+    );
+}
+
+#[test]
+fn only_whitespace() {
+    scan("\r").unwrap();
+}
+
+#[test]
+fn test_scan_string_verb_string() {
+    let tokens = scan("'abc','def'").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(
+        tokens,
+        [
+            Token::LitString(String::from("abc")),
+            Token::Primitive(String::from(",")),
+            Token::LitString(String::from("def")),
+        ]
+    );
+}
+
+#[test]
+fn test_scan_name_verb_name_not_spaced() {
+    let tokens = scan("foo+bar\n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(
+        tokens,
+        [
+            Token::Name(String::from("foo")),
+            Token::Primitive(String::from("+")),
+            Token::Name(String::from("bar")),
+        ]
+    );
+}
+
+#[test]
+fn test_scan_primitives() {
+    let tokens = scan("a. I. 'A' \n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(
+        tokens,
+        [
+            Token::Primitive(String::from("a.")),
+            Token::Primitive(String::from("I.")),
+            Token::LitString(String::from("A")),
+        ]
+    );
+}
+
+#[test]
+fn test_scan_primitives_not_spaced() {
+    let tokens = scan("a.I.'A' \n").unwrap();
+    println!("{:?}", tokens);
+    assert_eq!(
+        tokens,
+        [
+            Token::Primitive(String::from("a.")),
+            Token::Primitive(String::from("I.")),
+            Token::LitString(String::from("A")),
+        ]
+    );
 }
