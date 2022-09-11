1ac13743ee1727fa96257f0bc62ca6babd74fef9
Aaron Ash <aaron.ash@gmail.com>
2022-09-11 17:43:42 +1000

    fix: use array promotion in minus and times

--- src/verbs.rs
+++ src/verbs.rs
@@ -121,38 +121,54 @@ pub fn v_plus(x: Option<&Word>, y: &Word) -> Result<Word, JError> {
 }
 
 pub fn v_minus(x: Option<&Word>, y: &Word) -> Result<Word, JError> {
-    //TODO use promotion()
     match x {
         None => Err(JError {
             message: "monadic - not implemented yet".to_string(),
         }),
-        Some(x) => {
-            if let (Word::Noun(IntArray { a: x }), Word::Noun(IntArray { a: y })) = (x, y) {
-                Ok(Word::Noun(IntArray { a: x - y }))
-            } else {
-                Err(JError {
-                    message: "minus not supported for these types yet".to_string(),
-                })
-            }
-        }
+        Some(x) => match (x, y) {
+            (Word::Noun(x), Word::Noun(y)) => match promotion(x, y) {
+                Ok((IntArray { a: x }, IntArray { a: y })) => Ok(Word::Noun(IntArray { a: x - y })),
+                Ok((ExtIntArray { a: x }, ExtIntArray { a: y })) => {
+                    Ok(Word::Noun(ExtIntArray { a: x - y }))
+                }
+                Ok((FloatArray { a: x }, FloatArray { a: y })) => {
+                    Ok(Word::Noun(FloatArray { a: x - y }))
+                }
+                Err(e) => Err(e),
+                _ => Err(JError {
+                    message: "domain error".to_string(),
+                }),
+            },
+            _ => Err(JError {
+                message: "minus not supported for these types yet".to_string(),
+            }),
+        },
     }
 }
 
 pub fn v_times(x: Option<&Word>, y: &Word) -> Result<Word, JError> {
-    //TODO use promotion()
     match x {
         None => Err(JError {
             message: "monadic * not implemented yet".to_string(),
         }),
-        Some(x) => {
-            if let (Word::Noun(IntArray { a: x }), Word::Noun(IntArray { a: y })) = (x, y) {
-                Ok(Word::Noun(IntArray { a: x * y }))
-            } else {
-                Err(JError {
-                    message: "times not supported for these types yet".to_string(),
-                })
-            }
-        }
+        Some(x) => match (x, y) {
+            (Word::Noun(x), Word::Noun(y)) => match promotion(x, y) {
+                Ok((IntArray { a: x }, IntArray { a: y })) => Ok(Word::Noun(IntArray { a: x * y })),
+                Ok((ExtIntArray { a: x }, ExtIntArray { a: y })) => {
+                    Ok(Word::Noun(ExtIntArray { a: x * y }))
+                }
+                Ok((FloatArray { a: x }, FloatArray { a: y })) => {
+                    Ok(Word::Noun(FloatArray { a: x * y }))
+                }
+                Err(e) => Err(e),
+                _ => Err(JError {
+                    message: "domain error".to_string(),
+                }),
+            },
+            _ => Err(JError {
+                message: "plus not supported for these types yet".to_string(),
+            }),
+        },
     }
 }
 
