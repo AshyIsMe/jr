42f5072d8a46da38fd26bc012e6e7cb1d9ffb203
Chris West (Faux) <git@goeswhere.com>
2022-11-06 14:39:09 +0000

    chore: unused cow utility methods

--- src/arrays/cow.rs
+++ src/arrays/cow.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use ndarray::prelude::*;
+use ndarray::IntoDimension;
 use num::complex::Complex64;
 use num::{BigInt, BigRational};
 
@@ -42,12 +44,46 @@ impl<'v> JArrayCow<'v> {
         impl_array!(self, ArrayBase::shape)
     }
 
+    pub fn to_shape(&self, shape: impl IntoDimension<Dim = IxDyn>) -> Result<JArrayCow> {
+        use JArrayCow::*;
+        Ok(match self {
+            BoolArray(a) => BoolArray(a.to_shape(shape)?),
+            CharArray(a) => CharArray(a.to_shape(shape)?),
+            IntArray(a) => IntArray(a.to_shape(shape)?),
+            ExtIntArray(a) => ExtIntArray(a.to_shape(shape)?),
+            RationalArray(a) => RationalArray(a.to_shape(shape)?),
+            FloatArray(a) => FloatArray(a.to_shape(shape)?),
+            ComplexArray(a) => ComplexArray(a.to_shape(shape)?),
+            BoxArray(a) => BoxArray(a.to_shape(shape)?),
+        })
+    }
+
     // TODO: Iterator
     pub fn outer_iter(&'v self) -> Vec<Self> {
         impl_array!(self, |x: &'v ArrayBase<_, _>| x
             .outer_iter()
             .map(|x| Self::from(x))
             .collect())
+    }
+
+    // TODO: Iterator
+    pub fn iter(&'v self) -> Vec<Self> {
+        use JArrayCow::*;
+        // AA TODO into_raw_vec() map, from
+        // impl_array!(self, |x: &'v ArrayBase<_, _>| x
+        //     .iter()
+        //     .map(|x| Self::from(x))
+        //     .collect())
+        match self {
+            BoolArray(a) => a.iter().map(|x| JArrayCow::from(*x)).collect(),
+            CharArray(a) => a.iter().map(|x| JArrayCow::from(*x)).collect(),
+            IntArray(a) => a.iter().map(|x| JArrayCow::from(*x)).collect(),
+            ExtIntArray(a) => a.iter().map(|x| JArrayCow::from(x.clone())).collect(),
+            RationalArray(a) => a.iter().map(|x| JArrayCow::from(x.clone())).collect(),
+            FloatArray(a) => a.iter().map(|x| JArrayCow::from(x.clone())).collect(),
+            ComplexArray(a) => a.iter().map(|x| JArrayCow::from(x.clone())).collect(),
+            BoxArray(a) => a.iter().map(|x| JArrayCow::from(x.clone())).collect(),
+        }
     }
 }
 
@@ -80,6 +116,24 @@ impl<'v> From<&'v JArray> for JArrayCow<'v> {
         }
     }
 }
+
+macro_rules! impl_from_atom {
+    ($t:ty, $j:path) => {
+        impl<'v> From<$t> for JArrayCow<'v> {
+            fn from(value: $t) -> JArrayCow<'v> {
+                $j(CowArrayD::from(ArrayD::from_elem(IxDyn(&[]), value)))
+            }
+        }
+    };
+}
+impl_from_atom!(u8, JArrayCow::BoolArray);
+impl_from_atom!(char, JArrayCow::CharArray);
+impl_from_atom!(i64, JArrayCow::IntArray);
+impl_from_atom!(BigInt, JArrayCow::ExtIntArray);
+impl_from_atom!(BigRational, JArrayCow::RationalArray);
+impl_from_atom!(f64, JArrayCow::FloatArray);
+impl_from_atom!(Complex64, JArrayCow::ComplexArray);
+impl_from_atom!(Word, JArrayCow::BoxArray);
 
 macro_rules! impl_from_nd {
     ($t:ty, $j:path) => {
