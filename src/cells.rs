b928cf24e76b7be096c62c4479747aaa98cd1ceb
Chris West (Faux) <git@goeswhere.com>
2022-11-16 11:59:51 +0000

    chore: remove spurious surplus_frame calculation

--- src/cells.rs
+++ src/cells.rs
@@ -36,14 +36,6 @@ pub fn generate_cells(
 
     let common_dims = common_dims(&x_frame, &y_frame);
     let common_frame = &x_shape[..common_dims];
-    let surplus_frame = if x_frame.len() > y_frame.len() {
-        &x_shape[common_dims..]
-    } else {
-        &y_shape[common_dims..]
-    };
-
-    debug!("common_frame: {:?}", common_frame);
-    debug!("surplus_frame: {:?}", surplus_frame);
 
     let x_surplus = &x_frame[common_dims..];
     let y_surplus = &y_frame[common_dims..];
@@ -57,6 +49,9 @@ pub fn generate_cells(
         // (true, true): it doesn't matter at all
         (true, true) | (false, true) => x_surplus,
     };
+
+    debug!("common_frame: {:?}", common_frame);
+    debug!("surplus_frame: {:?}", surplus_frame);
 
     let neg_common_frame_rank = -(common_frame.len() as i16);
     let x_macrocells = x.rank_iter(neg_common_frame_rank);
