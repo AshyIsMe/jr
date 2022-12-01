8de4dfc6ffdf53bb92b728218a00d205fa2eb7db
Chris West (Faux) <git@goeswhere.com>
2022-12-01 22:19:58 +0000

    feat: fill out atoms to lists

--- src/cells.rs
+++ src/cells.rs
@@ -147,6 +147,13 @@ pub fn flatten(results: &BoxArray) -> Result<JArray> {
         }
 
         match (arr.shape().len(), target_inner_shape.len()) {
+            (0, 1) => {
+                // atom
+                big_daddy.extend(arr.clone().into_elems());
+                for _ in 1..target_inner_shape[0] {
+                    big_daddy.push(Elem::Num(Num::zero()));
+                }
+            }
             (1, 1) => {
                 let current = arr.shape()[0];
                 let target = target_inner_shape[0];
