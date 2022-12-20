69b78f704f64c1ac141ef1b4182189bd410ceaee
Chris West (Faux) <git@goeswhere.com>
2022-12-20 21:18:40 +0000

    feat: comedy script load options

--- src/foreign/files.rs
+++ src/foreign/files.rs
@@ -1,6 +1,7 @@
 //! https://www.jsoftware.com/help/dictionary/dx001.htm
 
 use std::fs;
+use std::path::PathBuf;
 
 use anyhow::{anyhow, Context, Result};
 use itertools::Itertools;
@@ -9,31 +10,28 @@ use crate::{Arrayable, IntoJArray, JArray, JError, Word};
 
 // 1!:1
 pub fn f_read_file(y: &Word) -> Result<Word> {
-    match y {
-        Word::Noun(JArray::BoxArray(arr)) if arr.len() == 1 => {
-            let arr = arr
-                .iter()
-                .next()
-                .ok_or(JError::DomainError)
-                .context("empty box?")?;
-            let arr = arr
-                .when_char()
-                .ok_or(JError::NonceError)
-                .context("can't read boxed non-paths")?;
+    let path = arg_to_fs_path(y)?;
 
-            if arr.shape().len() > 1 {
-                return Err(JError::NonceError).context("multi-dimensional path");
-            }
-
-            let path: String = arr.iter().copied().collect();
-            match fs::read_to_string(&path) {
-                Ok(s) => Ok(s.chars().collect_vec().into_array()?.into_noun()),
-                Err(e) => Err(JError::FileNameError)
-                    .context(e)
-                    .with_context(|| anyhow!("reading {path:?}")),
-            }
-        }
-        _ => Err(JError::NonceError)
-            .context("can't read non-paths (hint: pointless box required 1!:1 <'a')"),
+    match fs::read_to_string(&path) {
+        Ok(s) => Ok(s.chars().collect_vec().into_array()?.into_noun()),
+        Err(e) => Err(JError::FileNameError)
+            .context(e)
+            .with_context(|| anyhow!("reading {path:?}")),
     }
+}
+
+pub fn arg_to_fs_path(y: &Word) -> Result<PathBuf> {
+    let Word::Noun(JArray::BoxArray(y)) = y else { return Err(JError::NonceError).context("only support <'filepath' loading"); };
+    if y.len() != 1 {
+        return Err(JError::NonceError).context("only one path please");
+    }
+
+    let path = y.iter().next().expect("just checked");
+    let JArray::CharArray(path) = path else { return Err(JError::NonceError).context("string required") };
+    if path.shape().len() != 1 {
+        return Err(JError::NonceError).context("single string required");
+    }
+
+    let path = path.iter().collect::<String>();
+    fs::canonicalize(&path).with_context(|| anyhow!("canonicalising {path}"))
 }
--- src/foreign/mod.rs
+++ src/foreign/mod.rs
@@ -10,7 +10,7 @@ use scripts::*;
 
 /// https://www.jsoftware.com/help/dictionary/xmain.htm
 pub fn foreign(ctx: &mut Ctx, l: usize, r: usize, _x: Option<&Word>, y: &Word) -> Result<Word> {
-    match (r, l) {
+    match (l, r) {
         (0, k) => f_load_script(ctx, k, y),
         (1, 1) => f_read_file(y).context("reading file"),
         _ => Err(JError::NonceError).with_context(|| anyhow!("unsupported foreign: {l}!:{r}")),
