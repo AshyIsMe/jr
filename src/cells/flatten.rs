c1ba429d9477abde1a61340656b97e34525545fa
Chris West (Faux) <git@goeswhere.com>
2022-12-22 18:06:18 +0000

    chore: set up push_with_shape for testing

--- src/cells/flatten.rs
+++ src/cells/flatten.rs
@@ -17,17 +17,7 @@ pub fn flatten(results: &BoxArray) -> Result<JArray> {
         .context("non-empty macrocells")?;
 
     // rank extend every child
-    let results = results.map(|arr| {
-        let rank_extended_shape = (0..max_rank - arr.shape().len())
-            .map(|_| &1)
-            .chain(arr.shape())
-            .copied()
-            .collect_vec();
-        // *not* into_shape, as into_shape returns errors for e.g. reversed arrays
-        arr.to_shape(rank_extended_shape)
-            .expect("rank extension is always valid")
-            .into_owned()
-    });
+    let results = results.map(|arr| rank_extend(max_rank, arr));
 
     // max each dimension
     let target_inner_shape = results
@@ -73,6 +63,19 @@ pub fn flatten(results: &BoxArray) -> Result<JArray> {
         .into_owned())
 }
 
+fn rank_extend(target: usize, arr: &JArray) -> JArray {
+    let rank_extended_shape = (0..target - arr.shape().len())
+        .map(|_| &1)
+        .chain(arr.shape())
+        .copied()
+        .collect_vec();
+
+    // *not* into_shape, as into_shape returns errors for e.g. reversed arrays
+    arr.to_shape(rank_extended_shape)
+        .expect("rank extension is always valid")
+        .into_owned()
+}
+
 fn push_with_shape(out: &mut Vec<Elem>, target: &[usize], arr: JArray) -> Result<()> {
     let shape = arr.shape();
     assert_eq!(shape.len(), target.len());
@@ -82,8 +85,8 @@ fn push_with_shape(out: &mut Vec<Elem>, target: &[usize], arr: JArray) -> Result
             let current = shape[0];
             let target = target[0];
             assert!(
-                current < target,
-                "{current} < {target}: single-dimensional fill can't see longer or equal shapes"
+                current <= target,
+                "{current} <= {target}: single-dimensional fill can't see longer shapes"
             );
             out.extend(arr.to_owned().into_elems());
             for _ in current..target {
@@ -109,4 +112,44 @@ fn push_with_shape(out: &mut Vec<Elem>, target: &[usize], arr: JArray) -> Result
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{arr0d, Elem, JArray};
+    use ndarray::{array, ArrayD};
+
+    fn push(target: &[usize], arr: ArrayD<i64>) -> Vec<i64> {
+        let mut out = Vec::new();
+        let arr = super::rank_extend(target.len(), &JArray::IntArray(arr));
+        super::push_with_shape(&mut out, target, arr).expect("push success");
+        out.into_iter()
+            .map(|c| match c {
+                Elem::Num(n) => n.value_i64().unwrap(),
+                _ => unreachable!("i64 arrays only"),
+            })
+            .collect()
+    }
+
+    #[test]
+    fn test_push() {
+        // not sure an atomic output is really legal, currently broken
+        // assert_eq!(vec![5], push(&[], arr0d(5)));
+        assert_eq!(vec![5], push(&[1], arr0d(5)));
+        assert_eq!(vec![5, 0], push(&[2], arr0d(5)));
+        assert_eq!(vec![5, 2, 0, 0, 0, 0], push(&[6], array![5, 2].into_dyn()));
+        assert_eq!(
+            vec![1, 2, 3, 4, 0, 0],
+            push(&[3, 2], array![[1, 2], [3, 4]].into_dyn())
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn test_push_multi_expand_2d() {
+        assert_eq!(
+            vec![1, 2, 0, 3, 4, 0, 0, 0, 0],
+            push(&[3, 3], array![[1, 2], [3, 4]].into_dyn())
+        );
+    }
 }
