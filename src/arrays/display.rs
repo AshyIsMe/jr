c4549da0d03ebf9c74f22f141e1297811f387a1b
Chris West (Faux) <git@goeswhere.com>
2023-01-02 20:07:06 +0000

    wip: spacing and layout

--- src/arrays/display.rs
+++ src/arrays/display.rs
@@ -3,8 +3,9 @@ use std::fmt;
 use std::fmt::Display;
 
 use ndarray::{ArrayBase, ArrayViewD};
+use unicode_width::UnicodeWidthStr;
 
-use crate::{impl_array, Elem, JArray};
+use crate::{impl_array, JArray};
 
 pub fn nd(mut f: impl fmt::Write, arr: &JArray) -> fmt::Result {
     match arr {
@@ -23,22 +24,32 @@ pub fn jsoft(mut f: impl fmt::Write, arr: &JArray) -> fmt::Result {
 
 // trait JFormat: Display {}
 
+fn width(s: impl AsRef<str>) -> usize {
+    s.as_ref().width()
+}
+
 fn br<T: Display>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
+    if arr.shape().is_empty() {
+        return write!(f, "{}", arr.first().expect("atom has an element"));
+    }
     let limit = 50usize;
     // TODO: jsoft takes from the end, not the start, for some reason
-    let iter = arr.rows().into_iter().take(limit);
+    let iter = arr.rows().into_iter().enumerate().take(limit);
     let table = iter
-        .map(|x| {
-            x.into_iter()
-                .take(limit)
-                .map(|x| format!("{x}"))
-                .collect_vec()
+        .map(|(p, x)| {
+            (
+                p,
+                x.into_iter()
+                    .take(limit)
+                    .map(|x| format!("{x}"))
+                    .collect_vec(),
+            )
         })
         .collect_vec();
 
-    let widths = table
+    let widths: Vec<usize> = table
         .iter()
-        .map(|row| row.iter().map(|s| s.chars().count()).collect_vec())
+        .map(|(_, row)| row.iter().map(width).collect_vec())
         .reduce(|l, r| {
             l.into_iter()
                 .zip(r.into_iter())
@@ -47,22 +58,43 @@ fn br<T: Display>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
         })
         .expect("non-empty rows");
 
-    for row in table {
-        write!(f, "{widths:?} {row:?}\n")?;
-    }
+    let multiples = arr
+        .shape()
+        .iter()
+        .rev()
+        .skip(1)
+        .fold(Vec::<usize>::new(), |mut acc, &x| {
+            let t: usize = acc.iter().product();
+            acc.push(x * t);
+            acc
+        });
 
-    Ok(())
-}
+    let last = table.last().expect("non-empty").0;
 
-fn js(f: &mut impl fmt::Write, shape: &[usize], items: &[Elem]) -> fmt::Result {
-    if shape.len() == 1 {
-        return write!(f, "{:?}", &items[..shape[0]]);
-    }
-    let (last, shape) = shape.split_last().expect("<= 1");
-    for x in 0..*last {
-        js(f, shape, &items[x * shape.iter().product::<usize>()..])?;
+    for (rn, row) in table {
+        for (col, (target, item)) in widths.iter().zip(row.into_iter()).enumerate() {
+            let len = width(&item);
+            write!(
+                f,
+                "{}",
+                (0..(target - len)).map(|_| ' ').collect::<String>()
+            )?;
+            if col != 0 {
+                write!(f, " ")?;
+            }
+            write!(f, "{item}")?;
+        }
         write!(f, "\n")?;
+        if rn == last {
+            break;
+        }
+        for &m in &multiples {
+            if (rn + 1) % m == 0 {
+                write!(f, "\n")?;
+            }
+        }
     }
+
     Ok(())
 }
 
@@ -84,6 +116,6 @@ mod tests {
             idot(12).into_shape(IxDyn(&[2, 3, 2])).unwrap().view(),
         )
         .unwrap();
-        assert_eq!("", s)
+        assert_eq!(" 0  1\n 2  3\n 4  5\n\n 6  7\n 8  9\n10 11\n", s)
     }
 }
