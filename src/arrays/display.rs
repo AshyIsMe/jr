e4ee7f2b5f9c396bc63a7ad41a474417c15be9a4
Chris West (Faux) <git@goeswhere.com>
2023-01-02 20:35:44 +0000

    feat: match the smoke format for non-box non-char

--- src/arrays/display.rs
+++ src/arrays/display.rs
@@ -1,8 +1,10 @@
 use itertools::Itertools;
 use std::fmt;
-use std::fmt::Display;
 
 use ndarray::{ArrayBase, ArrayViewD};
+use num::complex::Complex64;
+use num::{BigInt, BigRational};
+use num_traits::{One, Zero};
 use unicode_width::UnicodeWidthStr;
 
 use crate::{impl_array, JArray};
@@ -22,17 +24,31 @@ pub fn jsoft(mut f: impl fmt::Write, arr: &JArray) -> fmt::Result {
     }
 }
 
-// trait JFormat: Display {}
+trait JFormat {
+    fn j_format(&self) -> String;
+}
 
 fn width(s: impl AsRef<str>) -> usize {
     s.as_ref().width()
 }
 
-fn br<T: Display>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
+fn br<T: JFormat>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
     if arr.shape().is_empty() {
-        return write!(f, "{}", arr.first().expect("atom has an element"));
+        return write!(
+            f,
+            "{}\n",
+            arr.first().expect("atom has an element").j_format()
+        );
     }
-    let limit = 50usize;
+    if arr.is_empty() {
+        // what on earth is even going on
+        return if arr.shape().len() == 1 {
+            write!(f, "\n")
+        } else {
+            write!(f, "")
+        };
+    }
+    let limit = 128usize;
     // TODO: jsoft takes from the end, not the start, for some reason
     let iter = arr.rows().into_iter().enumerate().take(limit);
     let table = iter
@@ -41,7 +57,7 @@ fn br<T: Display>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
                 p,
                 x.into_iter()
                     .take(limit)
-                    .map(|x| format!("{x}"))
+                    .map(|x| x.j_format())
                     .collect_vec(),
             )
         })
@@ -96,6 +112,77 @@ fn br<T: Display>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
     }
 
     Ok(())
+}
+
+macro_rules! j_format_is_display {
+    ($t:ty) => {
+        impl JFormat for $t {
+            fn j_format(&self) -> String {
+                format!("{self}")
+            }
+        }
+    };
+}
+
+j_format_is_display!(u8);
+j_format_is_display!(char);
+
+#[inline]
+fn sign_lift<T: JFormat + num_traits::sign::Signed>(val: T, f: impl FnOnce(T) -> String) -> String {
+    if val.is_negative() {
+        format!("_{}", f(-val))
+    } else {
+        f(val)
+    }
+}
+
+impl JFormat for i64 {
+    fn j_format(&self) -> String {
+        sign_lift(*self, |v| format!("{v}"))
+    }
+}
+impl JFormat for BigInt {
+    fn j_format(&self) -> String {
+        // TODO: incredibly lazy clone
+        sign_lift(self.clone(), |v| format!("{v}"))
+    }
+}
+
+impl JFormat for f64 {
+    fn j_format(&self) -> String {
+        sign_lift(*self, |v| {
+            if v.is_infinite() {
+                format!("_")
+            } else {
+                format!("{v}")
+            }
+        })
+    }
+}
+
+impl JFormat for JArray {
+    fn j_format(&self) -> String {
+        unreachable!()
+    }
+}
+
+impl JFormat for BigRational {
+    fn j_format(&self) -> String {
+        if self.denom().is_one() {
+            self.numer().j_format()
+        } else {
+            format!("{}r{}", self.numer().j_format(), self.denom().j_format())
+        }
+    }
+}
+impl JFormat for Complex64 {
+    fn j_format(&self) -> String {
+        if self.im.is_zero() {
+            self.re.j_format()
+        } else {
+            format!("{}j{}", self.re.j_format(), self.im.j_format())
+        }
+    }
 }
 
 #[cfg(test)]
