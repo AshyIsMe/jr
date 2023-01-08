433436e0b96b3b7c0a102fdbbe4c1f937eb7483e
Aaron Ash <aaron.ash@gmail.com>
2023-01-08 15:37:26 +1000

    wip: br_box() but worse

--- src/arrays/display.rs
+++ src/arrays/display.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use log::debug;
 use std::cmp::max;
 use std::fmt;
 
@@ -19,7 +20,8 @@ pub fn nd(mut f: impl fmt::Write, arr: &JArray) -> fmt::Result {
 
 pub fn jsoft(mut f: impl fmt::Write, arr: &JArray) -> fmt::Result {
     match arr {
-        JArray::BoxArray(arr) => br_box(f, arr.view()),
+        //JArray::BoxArray(arr) => br_box(f, arr.view()),
+        JArray::BoxArray(arr) => br_box_aa_wrong(f, arr.view()),
         JArray::CharArray(arr) => br_char(f, arr.view()),
         _ => impl_array!(arr, |arr: &ArrayBase<_, _>| br(&mut f, arr.view())),
     }
@@ -109,7 +111,109 @@ fn br<T: JFormat>(mut f: impl fmt::Write, arr: ArrayViewD<T>) -> fmt::Result {
     Ok(())
 }
 
-fn br_box(mut f: impl fmt::Write, arr: ArrayViewD<JArray>) -> fmt::Result {
+// fn br_box(mut f: impl fmt::Write, arr: ArrayViewD<JArray>) -> fmt::Result {
+//     if let Some(s) = short_array_cases(&arr) {
+//         return write!(f, "{s}");
+//     }
+//     let corners = ['┌', '┐', '└', '┘'];
+//     let walls = ['─', '│', '┼'];
+//     let limit = 128usize;
+//     // TODO: jsoft takes from the end, not the start, for some reason
+//     let iter = arr.rows().into_iter().enumerate().take(limit);
+//     let table = iter
+//         .map(|(p, x)| {
+//             (
+//                 p,
+//                 x.into_iter()
+//                     .take(limit)
+//                     .map(|x| x.j_format())
+//                     .collect_vec(),
+//             )
+//         })
+//         .collect_vec();
+
+//     let widths: Vec<usize> = table
+//         .iter()
+//         .map(|(_, row)| {
+//             row.iter()
+//                 .map(|item| item.split('\n').map(width).max().unwrap_or_default())
+//                 .collect_vec()
+//         })
+//         .reduce(|l, r| {
+//             l.into_iter()
+//                 .zip(r.into_iter())
+//                 .map(|(l, r)| max(l, r))
+//                 .collect_vec()
+//         })
+//         .expect("non-empty rows");
+
+//     let widths: Vec<usize> = table
+//         .iter()
+//         .map(|(_, row)| {
+//             row.iter()
+//                 .map(|item| item.split('\n').map(width).max().unwrap_or_default())
+//                 .collect_vec()
+//         })
+//         .reduce(|l, r| {
+//             l.into_iter()
+//                 .zip(r.into_iter())
+//                 .map(|(l, r)| max(l, r))
+//                 .collect_vec()
+//         })
+//         .expect("non-empty rows");
+
+//     // let row_sizes = table
+//     //     .iter()
+//     //     .map(|(_, row)| {
+//     //         row.iter()
+//     //             .map(|item| {
+//     //                 let lines = item.split('\n').count();
+//     //                 let max_width = item
+//     //                     .split('\n')
+//     //                     .map(|line| width(line))
+//     //                     .max()
+//     //                     .unwrap_or_default();
+//     //                 (lines, max_width)
+//     //             })
+//     //             .collect_vec()
+//     //     }).collect_vec();
+//     // let widths = row_sizes.iter().map(|(_lines, width)| width)
+//     //     .reduce(|l, r| {
+//     //         l.into_iter()
+//     //             .zip(r.into_iter())
+//     //             .map(|(l, r)| *l.max(r))
+//     //             .collect_vec()
+//     //     })
+//     //     .expect("non-empty rows");
+
+//     let multiples = compute_dimension_spacing(&arr);
+
+//     let last = table.last().expect("non-empty").0;
+
+//     for (rn, row) in table {
+//         for (col, (target, item)) in row_sizes.iter().zip(row.into_iter()).enumerate() {
+//             let len = width(&item);
+//             write!(
+//                 f,
+//                 "{}",
+//                 (0..(target - len)).map(|_| ' ').collect::<String>()
+//             )?;
+//             if col != 0 {
+//                 write!(f, " ")?;
+//             }
+//             write!(f, "{item}")?;
+//         }
+//         write!(f, "\n")?;
+//         if rn == last {
+//             break;
+//         }
+//         print_dimension_markings(&mut f, rn, &multiples)?;
+//     }
+
+//     Ok(())
+// }
+
+fn br_box_aa_wrong(mut f: impl fmt::Write, arr: ArrayViewD<JArray>) -> fmt::Result {
     if let Some(s) = short_array_cases(&arr) {
         return write!(f, "{s}");
     }
@@ -130,7 +234,17 @@ fn br_box(mut f: impl fmt::Write, arr: ArrayViewD<JArray>) -> fmt::Result {
         })
         .collect_vec();
 
-    let widths: Vec<usize> = table
+    let row_max_heights: Vec<usize> = table
+        .iter()
+        .map(|(_, row)| {
+            row.iter()
+                .map(|item| item.split('\n').count())
+                .max()
+                .unwrap_or_default()
+        })
+        .collect_vec();
+
+    let col_max_widths: Vec<usize> = table
         .iter()
         .map(|(_, row)| {
             row.iter()
@@ -145,67 +259,49 @@ fn br_box(mut f: impl fmt::Write, arr: ArrayViewD<JArray>) -> fmt::Result {
         })
         .expect("non-empty rows");
 
-    let widths: Vec<usize> = table
+    debug!("arr.shape(): {:?}", arr.shape());
+    debug!("table: {:?}", table);
+    debug!("row_max_heights: {:?}", row_max_heights);
+    debug!("col_max_widths: {:?}", col_max_widths);
+
+    // pad each item according to row_max_heights, col_max_widths
+    // AA TODO each box should be correct in isolation but the inputs seem wrong already
+    let boxes = table
         .iter()
-        .map(|(_, row)| {
+        .map(|(rn, row)| {
             row.iter()
-                .map(|item| item.split('\n').map(width).max().unwrap_or_default())
+                .enumerate()
+                .map(|(cn, item)| {
+                    let lines = item.split('\n').collect_vec();
+                    let target_height = row_max_heights[*rn];
+                    let target_width = col_max_widths[cn];
+                    let missing_height = target_height - lines.len();
+                    let lines = lines
+                        .iter()
+                        .chain(std::iter::repeat(&" \n").take(missing_height))
+                        .collect_vec();
+                    let lines = lines
+                        .iter()
+                        .map(|l| {
+                            debug!("l: {:?}", l);
+                            let missing_width = target_width - l.len();
+                            let tail: String = std::iter::repeat(" ").take(missing_width).collect();
+                            vec![l.to_string(), tail].join("")
+                        })
+                        .collect_vec();
+                    lines.join("\n")
+                })
                 .collect_vec()
         })
-        .reduce(|l, r| {
-            l.into_iter()
-                .zip(r.into_iter())
-                .map(|(l, r)| max(l, r))
-                .collect_vec()
-        })
-        .expect("non-empty rows");
+        .collect_vec();
 
-    // let row_sizes = table
-    //     .iter()
-    //     .map(|(_, row)| {
-    //         row.iter()
-    //             .map(|item| {
-    //                 let lines = item.split('\n').count();
-    //                 let max_width = item
-    //                     .split('\n')
-    //                     .map(|line| width(line))
-    //                     .max()
-    //                     .unwrap_or_default();
-    //                 (lines, max_width)
-    //             })
-    //             .collect_vec()
-    //     }).collect_vec();
-    // let widths = row_sizes.iter().map(|(_lines, width)| width)
-    //     .reduce(|l, r| {
-    //         l.into_iter()
-    //             .zip(r.into_iter())
-    //             .map(|(l, r)| *l.max(r))
-    //             .collect_vec()
-    //     })
-    //     .expect("non-empty rows");
+    debug!("boxes: {:?}", boxes);
+    // AA TODO Now join each box together into rows/cols with box chars surrounding them
 
-    let multiples = compute_dimension_spacing(&arr);
-
-    let last = table.last().expect("non-empty").0;
-
-    for (rn, row) in table {
-        for (col, (target, item)) in row_sizes.iter().zip(row.into_iter()).enumerate() {
-            let len = width(&item);
-            write!(
-                f,
-                "{}",
-                (0..(target - len)).map(|_| ' ').collect::<String>()
-            )?;
-            if col != 0 {
-                write!(f, " ")?;
-            }
-            write!(f, "{item}")?;
+    for (i, row) in table.iter() {
+        for item in row.iter() {
+            write!(f, "{item}");
         }
-        write!(f, "\n")?;
-        if rn == last {
-            break;
-        }
-        print_dimension_markings(&mut f, rn, &multiples)?;
     }
 
     Ok(())
